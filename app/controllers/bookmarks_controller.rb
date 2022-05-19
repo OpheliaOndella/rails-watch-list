b4bbc71d32aee679d181b14b6b21875dbbb96017
ophelia ondella <ophelia.ondella@gmail.com>
2022-05-19 16:38:49 +0200

    Watch list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,11 @@
 class BookmarksController < ApplicationController
+  def new
+    @new_bookmark = List.new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,24 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show() end
+
+  def new
+    @list = Lists.new
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
 end
