bedb9b14de82549ebcf3ce521f21537425947b88
ophelia ondella <ophelia.ondella@gmail.com>
2022-05-19 14:39:51 +0200

    add controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :lists, through: :bookmarks
+  validates :title, uniqueness: true
+  validates :title, :overview, presence: true
   has_many :bookmarks
 end
